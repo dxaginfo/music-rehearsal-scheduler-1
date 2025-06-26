a5dccc6ae6e11244fc97d8f5264b94c2591b85ef
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-26 08:22:57 -0300

    Add basic React front-end code

---
+++ frontend/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <div>
+      <h1>Music Rehearsal Scheduler</h1>
+      <p>Welcome. This is a web app to schedule band rehearsals and track attendance.</p>
+    </div>
+  );
+}
+export default App;
